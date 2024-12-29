cf1b5b31c2a8a908682810de369bdfe2f54a5b4c
hjw9504 <hjw9504@naver.com>
2024-12-29 23:37:05 +0900

    feat: spring batch, async config 추가

--- src/main/java/com/project/spring_batch/JobConfig.java
+++
@@ -1,5 +0,0 @@
-package com.project.spring_batch;
-
-public class JobConfig {
-    
-}
---
+++ src/main/java/com/project/spring_batch/config/JobConfig.java
@@ -0,0 +1,43 @@
+package com.project.spring_batch.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+public class JobConfig {
+
+    @Bean
+    public Job SpringJob(PlatformTransactionManager transactionManager, JobRepository jobRepository) {
+        return new JobBuilder("springJob", jobRepository)
+                .start(initJob(transactionManager, jobRepository))
+                .build();
+    }
+
+    @Bean
+    public Step initJob(PlatformTransactionManager transactionManager, JobRepository jobRepository) {
+        log.info("Init Job");
+        return new StepBuilder("initJob", jobRepository)
+                .tasklet(firstJob(), transactionManager)
+                .build();
+    }
+
+    public Tasklet firstJob() {
+        return (contribution, chunkContext) -> {
+            log.info("First Job");
+            return RepeatStatus.FINISHED;
+        };
+    }
+}
